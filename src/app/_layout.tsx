67de824e110af4a53485c342e36ea7f0b3b2f883
iamdaviddev <davidpaulodev@gmail.com>
2025-09-15 19:09:56 +0100

    initial commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+import { DarkTheme, ThemeProvider } from  '@react-navigation/native'
+
+export default function RootLayout() {
+  return <ThemeProvider value={DarkTheme}>
+    <Stack>
+      <Stack.Screen name="index" options={{ title: 'Events', headerLargeTitle: true }} />
+    </Stack>
+  </ThemeProvider>
+}
